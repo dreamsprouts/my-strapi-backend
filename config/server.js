6924b7eddba91bafb0ff4a5685ea56302939bad8
Morris Li <morrisli@Morriss-MacBook-Pro.local>
2024-08-20 15:51:46 +0800

    Configured fixed

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['ff8b1fff75d7135cde7c7f8a96b199e3b8aeb3ce969be8dd1d9b3458408649cb']),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
