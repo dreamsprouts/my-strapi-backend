2cec5666ef2309701e93a549999c427a5a5e78e5
Morris Li <morrisli@Morriss-MacBook-Pro.local>
2024-08-20 15:37:58 +0800

    Configured app.keys and jwtSecret for Strapi

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'jdgYSp6B4Z+jmP9icFLADQ=='),
+      },
+    },
+  });
+  
